e41f81d490b402302a088a02dd9751645a9f858d
MicaelaRojas <67761871+MicaelaRojas@users.noreply.github.com>
2024-09-18 01:36:33 +0000

    Arreglos a home y navbar

--- src/front/js/pages/carrusel-imagenes.js
+++ src/front/js/pages/carrusel-imagenes.js
@@ -1,37 +1,37 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-
-// import { Context } from "../store/appContext";
-import '../../styles/home.css';
+import React from "react";
+import '../../styles/carrusel-imagenes.css';
+import carrusel1 from "../../img/carrousel1.jpg";
+import carrusel2 from "../../img/carrousel2.jpg";
+import carrusel3 from "../../img/carrousel3.jpg";
 
 export const Carrusel = () => {
-	// const { store, actions } = useContext(Context);
-	return (
-		<div id="carouselExampleIndicators" className="carousel slide">
-			<div className="carousel-indicators">
-				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-				<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-			</div>
-			<div className="carousel-inner">
-				<div className="carousel-item active">
-					<img src="https://img.freepik.com/fotos-premium/astronauta-luna-trabajando-inteligencia-artificial-generativa-portatil_886951-766.jpg" className="d-block w-100 carrusel" alt="carrusel1" />
-				</div>
-				<div className="carousel-item">
-					<img src="https://www.technomag.fr/wp-content/uploads/2023/11/tomorrowland-2023-bresil-960x1000.png" className="d-block w-100 carrusel" alt="carrusel2" />
-				</div>
-				<div className="carousel-item">
-					<img src="https://img.freepik.com/fotos-premium/planeta-espacio-ia-generativa_445983-2320.jpg" className="d-block w-100 carrusel" alt="carrusel3" />
-				</div>
-			</div>
-			<button className="icon-button icon-button-left" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-				<i className="fa-solid fa-angle-left"></i> {/* Ícono rotado a la izquierda*/}
-				<span className="visually-hidden">Previous</span>
-			</button>
-			<button className="icon-button icon-button-right" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-				<i className="fa-solid fa-angle-right"></i>	{/* Ícono rotado a la derecha */}
-				<span className="visually-hidden">Next</span>
-			</button>
-		</div>
-	);
+  return (
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+<div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+
+      <img src={carrusel2} class="d-block w-100" />
+        </div>
+        <div className="carousel-item">
+          <img src={carrusel1}  class="d-block w-100" />
+        </div>
+        <div className="carousel-item">
+          <img src={carrusel3}  class="d-block w-100" />
+		  </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+  );
 };
