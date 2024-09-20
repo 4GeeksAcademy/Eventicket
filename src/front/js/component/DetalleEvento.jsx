f0888315e64cad8c74fc5e776eb648cc7ce46f37
MicaelaRojas <67761871+MicaelaRojas@users.noreply.github.com>
2024-09-20 22:39:23 +0000

    Probando Paypal

--- src/front/js/component/DetalleEvento.jsx
+++ src/front/js/component/DetalleEvento.jsx
@@ -3,8 +3,6 @@ import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 import "../../styles/detalleEvento.css";
 
-//Hola
-
 export const DetalleEvento = () => {
   const { eventId } = useParams();
   const { store } = useContext(Context);
