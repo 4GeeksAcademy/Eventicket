b2fe0b19aa302595905a1a1ae99c261737b9b656
rociomr08 <161978284+rociomr08@users.noreply.github.com>
2024-09-25 12:42:54 +0000

    estilos en favoritos y detalle evento

--- src/front/js/component/FavoritosUser.jsx
+++ src/front/js/component/FavoritosUser.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import "../../styles/CardEvento.css";
 
 const FavoritosUser = () => {
-
     const { store, actions } = useContext(Context);
-
 
     useEffect(() => {
         actions.getFavourites();
@@ -15,30 +14,30 @@ const FavoritosUser = () => {
 
     return (
         <div className="container py-5">
-            <h1 className="text-center">Mis Favoritos</h1>
+            <h1 className="text-center fs-1 text-primary">Mis Favoritos</h1>
             <div className="container mt-4">
-                <div className="row">
-                    {favourites.map((favorito, index) => (
-                        <div className="col-md-4 mb-4" key={index}>
-                            <div className="card">
+                <div className="row row-cols-1 row-cols-md-3 g-4 py-3">
+                    {favourites.map((favorito) => (
+                        <div className="col-md-4 mb-4" key={favorito.id}>
+                            <div className="card image-container h-100 shadow-sm">
                                 <img
                                     src={favorito.image_url || "https://picsum.photos/300/200/"}
                                     className="card-img-top"
                                     alt={favorito.title}
-                                    style={{ height: "200px", objectFit: "cover" }}
+                                    style={{ height: "230px", objectFit: "cover" }}
                                 />
                                 <div className="card-body">
-                                    <h5 className="card-title text-primary mb-2">{favorito.title}</h5>
+                                    <h4 className="card-title text-primary mb-2">{favorito.title}</h4>
                                     <div className="d-flex justify-content-between align-items-center mb-2">
                                         <div className="d-flex align-items-center">
                                             <i className="fa-solid fa-location-dot px-2 fs-4"></i>
                                             <span>{favorito.location}</span>
                                         </div>
                                         <button
-                                            className="btn btn-outline-warning"
+                                            className="btn btn-outline-danger"
                                             onClick={() => actions.deleteFavourite(favorito.id)}
                                         >
-                                            <i className="fa fa-heart-o px-2" aria-hidden="true"></i> Quitar
+                                            <i className="fa fa-trash-o" aria-hidden="true"></i> Quitar
                                         </button>
                                     </div>
                                     <div className="d-flex align-items-center fs-5 mb-2">
@@ -49,12 +48,14 @@ const FavoritosUser = () => {
                                         <i className="fa-regular fa-clock px-2"></i>
                                         <span>{favorito.time}</span>
                                     </div>
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        <h6>S/. {favorito.price}</h6>
-                                        <Link to={`/detalle/${favorito.id}`}>
-                                            <button className="btn btn-success">Ver detalles</button>
-                                        </Link>
-                                    </div>
+                                </div>
+                                <div className="card-footer bg-white border-top-0 d-flex justify-content-between align-items-center mb-3">
+                                    <h6 className="my-0 text-primary fs-4 fw-bolder">S/. {favorito.price}</h6>
+                                    <Link to={`/detalle/${favorito.id}`}>
+                                        <button className="btn boton-verde rounded-pill fw-bold">
+                                            Ver detalles
+                                        </button>
+                                    </Link>
                                 </div>
                             </div>
                         </div>
@@ -64,4 +65,5 @@ const FavoritosUser = () => {
         </div>
     );
 };
+
 export default FavoritosUser;
