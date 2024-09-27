0c011a9e395d0fe3c21b6c1cec4c94609299ac9f
MicaelaRojas <67761871+MicaelaRojas@users.noreply.github.com>
2024-09-27 02:18:49 +0000

    ACTUALIZADO FINAL

--- src/front/js/pages/carrusel-imagenes.js
+++ src/front/js/pages/carrusel-imagenes.js
@@ -5,6 +5,20 @@ import carrusel2 from "../../img/carrousel2.jpg";
 import carrusel3 from "../../img/carrousel3.jpg";
 
 export const Carrusel = () => {
+  let swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 15,
+      strech: 0,
+      depth: 300,
+      modifier: 1,
+      slideShadow: true,
+    },
+    loop: true,
+  })
   return (
     <div id="carouselExampleIndicators" className="carousel slide mb-2" data-bs-ride="carousel" data-bs-interval="3000">
       <div className="carousel-indicators">
@@ -32,6 +46,57 @@ export const Carrusel = () => {
         <span className="carousel-control-next-icon" aria-hidden="true"></span>
         <span className="visually-hidden">Next</span>
       </button>
-    </div>
+    // </div>
+
+
+    //LO DE LUIS AQUI
+    // <div className="swiper mySwiper my-3">
+    //   <div className="swiper-wrapper">
+
+    //     <div className="swiper-slide">
+    //       <img className="img-carrusel" src="https://www.miraflores.gob.pe/wp-content/uploads/2022/08/D23FBC35-C386-47C9-AD69-6E2AFF1443D2-1024x604.jpeg" alt="bellas artes en miraflores"></img>
+    //     </div>
+ 
+    //     <div className="swiper-slide">
+    //       <img className="img-carrusel" src="https://workedmusic.com/wp-content/uploads/2022/12/20221219_154917_0000.png" alt="concierto"></img>
+    //     </div>
+
+    //     <div className="swiper-slide">
+    //       <img className="img-carrusel" src="https://i0.wp.com/www.pachaindah.com/html/locales-eventos-lima.jpg?resize=798%2C442&ssl=1" alt="evento de aniversario"></img>
+    //     </div>
+
+    //     <div className="swiper-slide">
+    //       <img className="img-carrusel" src="https://grandluxorhotels.com/wp-content/uploads/2016/09/9323706488_7c288a9659_b.jpg" alt="evento empresarial"></img>
+    //     </div>
+   
+    //     <div className="swiper-slide">
+    //       <img className="img-carrusel" src="https://news.microsoft.com/wp-content/uploads/prod/sites/61/2024/06/Alberto-Granados-hablando-frente-al-publico-de-la-plenaria-en-Microsoft-Envision-AI-Connection-1200x630.jpg" alt="evento de microsoft"></img>
+    //     </div>
+
+    //     <div className="swiper-slide">
+    //       <img className="img-carrusel" src="https://grandluxormice.com/es/wp-content/uploads/sites/3/2022/07/Evento-corporativo-e1661944165280-1900x1069.jpg" alt="evento emprendimiento"></img>
+    //     </div>
+
+    //     <div className="swiper-slide">
+    //       <img className="img-carrusel" src="https://cevents.es/wp-content/uploads/2021/11/evento-corporativo-imgpost.jpg" alt="evento de oratoria"></img>
+    //     </div>
+
+    //     <div className="swiper-slide">
+    //       <img className="img-carrusel" src="https://i0.wp.com/www.pachaindah.com/html/locales-eventos-lima.jpg?resize=798%2C442&ssl=1" alt="evento de aniversario"></img>
+    //     </div>
+
+    //     <div className="swiper-slide">
+    //       <img className="img-carrusel" src="https://grandluxormice.com/es/wp-content/uploads/sites/3/2022/07/Evento-corporativo-e1661944165280-1900x1069.jpg" alt="evento emprendimiento"></img>
+    //     </div>
+
+    //     <div className="swiper-slide">
+    //       <img className="img-carrusel" src="https://grandluxorhotels.com/wp-content/uploads/2016/09/9323706488_7c288a9659_b.jpg" alt="evento empresarial"></img>
+    //     </div>
+
+    //     <div className="swiper-slide">
+    //       <img className="img-carrusel" src="https://news.microsoft.com/wp-content/uploads/prod/sites/61/2024/06/Alberto-Granados-hablando-frente-al-publico-de-la-plenaria-en-Microsoft-Envision-AI-Connection-1200x630.jpg" alt="evento de microsoft"></img>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
