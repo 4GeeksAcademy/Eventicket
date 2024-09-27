af434aee8a943a0ab6a145af6a35296a156c897d
Alejosz1902 <111547538+Alejosz1902@users.noreply.github.com>
2024-09-27 14:24:24 +0000

    path

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 const Dotenv = require('dotenv-webpack');
 module.exports = merge(common, {
     mode: 'production',
